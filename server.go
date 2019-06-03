65d5e97e927f5de9040383105b3ff30a98ff734c
andrewmathies <andrew.mathies@geappliances.com>
2019-06-03 10:39:53 -0400

    dont need mux

--- server.go
+++ server.go
@@ -6,8 +6,13 @@ import (
 	"net/http/httputil"
 	"html"
 	"log"
+	"context"
+	"crypto/tls"
+	"flag"
+	"io"
+	"time"
 
-	"github.com/gorilla/mux"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var dict map[string]Unit
@@ -45,15 +50,50 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO convert dict to json, send that as response
 }
 
-func main() {
-	dict = make(map[string]Unit)
-	
-	log.Println("Starting server")
-
-	router := mux.NewRouter().StrictSlash(true)
+func makeHTTPServer() *http.Server {
+	router := &http.ServeMux{}
+	//mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/unit/{beanID}", unitHandler)
 	router.HandleFunc("/units/{labID}", listHandler)
-	router.PathPrefix("/lab/{labID}").Handler(http.FileServer(http.Dir("./static")))
+	router.Handle("/lab/{labID}", http.StripPrefix("/lab/{labID}/", http.FileServer(http.Dir("./static"))))
+	//router.PathPrefix("/lab/{labID}").Handler(http.FileServer(http.Dir("./static")))
 
-	log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", router))
+	return &http.Server{
+        ReadTimeout:  5 * time.Second,
+        WriteTimeout: 5 * time.Second,
+        IdleTimeout:  120 * time.Second,
+        Handler:      router,
+    }
+}
+
+func main() {
+	dict = make(map[string]Unit)
+
+	var m *autocert.Manager
+	var server *http.Server
+
+	hostPolicy = func(ctx context.Context, host string) error {
+		allowedHost = "www.sbc.crabdance.com"
+		if host == allowedHost {
+			return nil
+		}
+
+		return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+	}
+
+	certPath := "/etc/letsencrypt/live/sbc.crabdance.com/"
+	m = &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: hostPolicy,
+		Cache:      autocert.DirCache(certPath),
+	}
+
+	server = makeHTTPServer()
+	server.Addr = ":443"
+	server.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+
+	go func() {
+		log.Println("Starting server on %s\n", server.Addr)
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	}()
 }
